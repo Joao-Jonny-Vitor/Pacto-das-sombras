0d94fe7572631c363a791053f9f3669c418946bb
rafael Campos <rafael.campos.m@hotmail.com>
2024-03-21 16:58:55 -0300

    Inicio do Script das habilidades

---
+++ ProjetoIntegrado/Assets/Scripts/MakeButton.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEditor.ShaderKeywordFilter;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MakeButton : MonoBehaviour
+{
+    [SerializeField]
+    private int physical;
+
+    private GameObject hero;
+
+
+    void Start()
+    {
+        string temp = gameObject.name;
+        gameObject.GetComponent<Button>().onClick.AddListener(() => AttachCallback(temp));
+        hero = GameObject.FindGameObjectWithTag("Hero");
+    }
+
+    private void AttachCallback(string btn)
+    {
+        if (btn.CompareTo("AttackBtn") == 0)
+        {
+            hero.GetComponent<FighterAction>().SelectAttack("melee");
+
+        }else if(btn.CompareTo("DefenseBtn") == 0)
+        {
+            hero.GetComponent<FighterAction>().SelectAttack("defense");
+
+        }else if(btn.CompareTo("AbilityBtn") ==0)
+        {
+            hero.GetComponent<FighterAction>().SelectAttack("ability");
+        }
+        else if(btn.CompareTo("MagicBtn") ==0) 
+
+        {
+            hero.GetComponent<FighterAction>().SelectAttack("magic");
+
+        }
+    }
+}
