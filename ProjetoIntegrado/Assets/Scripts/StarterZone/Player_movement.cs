50403e6e9c8efd02c637dc148105b92cc3a95889
João_Jonny_Vitor <joao.jonny.vitor@gmail.com>
2024-04-25 22:15:59 -0300

    colisaoPonte

--- ProjetoIntegrado/Assets/Scripts/StarterZone/Player_movement.cs
+++ ProjetoIntegrado/Assets/Scripts/StarterZone/Player_movement.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class Player_movement : MonoBehaviour
 {
     public InputActionReference move;
+    public InputActionReference Run;
     public Rigidbody2D rb;
     public float Speed;
     private Vector2 Direction;
@@ -20,4 +22,6 @@ public class Player_movement : MonoBehaviour
     {
         rb.velocity = new Vector2(Direction.x * Speed, Direction.y * Speed);
     }
+
+    
 }
