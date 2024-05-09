828a262b780c17bc0ae63c71e928842ef80ed040
João_Jonny_Vitor <joao.jonny.vitor@gmail.com>
2024-05-08 21:33:54 -0300

    scriptupdate

--- ProjetoIntegrado/Assets/Scripts/StarterZone/Player_movement.cs
+++ ProjetoIntegrado/Assets/Scripts/StarterZone/Player_movement.cs
@@ -1,27 +1,49 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Profiling;
 using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Interactions;
 
 public class Player_movement : MonoBehaviour
 {
-    public InputActionReference move;
-    public InputActionReference Run;
+    
     public Rigidbody2D rb;
-    public float Speed;
+    
     private Vector2 Direction;
 
-    
-    private void Update()
+    [SerializeField] float Speed;
+    private void Awake()
     {
-        Direction = move.action.ReadValue<Vector2>();
+        rb = GetComponent<Rigidbody2D>();
     }
-
+    
     private void FixedUpdate()
     {
-        rb.velocity = new Vector2(Direction.x * Speed, Direction.y * Speed);
+        rb.velocity = Direction * Speed;
     }
 
-    
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+            Debug.Log("Encostou no inimigo");
+        {
+           
+            Debug.Log("Interagiu");
+        }
+    }
+
+    private void OnInteraction (InputAction.CallbackContext context){
+
+    }
+   
+    public void OnMovement(InputAction.CallbackContext context){
+        Direction = context.ReadValue<Vector2>();
+
+    }
 }
+
+
